ae5170a348468e05b88eb8d28c22440456bc39c5
IxtiyorDeveloper <69586837+IxtiyorDeveloper@users.noreply.github.com>
2021-10-20 19:01:25 +0500

    dsdds

--- components/home/section-4/data/data.js
+++ components/home/section-4/data/data.js
@@ -1,12 +1,12 @@
 export default  [
     {
         id: 1,
-        name: "Abdullayev Toshqinboy",
-        text: "Men O’sh Davlat Texnologiya unversiteti “Komputer texnologiyalari”" +
-            " fakultetiga topshirdim. “Akbar Consulting “ firmasi yordami bilan talabalikka erishdim" +
-            " va menga 1 yillik sug’urta polisini sovg’a sifatida taqdim qildi. " +
-            "Men o’qish istagida bo’lgan yoshlarni “Akbar consulting “ firmasiga murojaat qilishlarini" +
-            " tavsiya qilaman.",
+        name: "Jumaniyazov Ruslan",
+        text: "Rossiyadagi xalqaro miqyosga ega “Sinergiya “ universitetini tanladim va " +
+            "unga o’qishga kirdim. Sizlarga ham shuni tavsiya qilaman. O’qishga kira olmadim" +
+            " deb tushkunlikka tushib" +
+            " o’tirmasdan xorijdagi nufuzli universitetlardan biriga “Akbar Consulting “ orqali" +
+            " o’qishga kiring.",
         img: "m1",
         url:"https://www.youtube.com/watch?v=eacD7gUbyKs"
     },
@@ -22,13 +22,13 @@ export default  [
     },
     {
         id: 3,
-        name: "Jumaniyazov Ruslan",
-        text: "Rossiyadagi xalqaro miqyosga ega “Sinergiya “ universitetini tanladim va " +
-            "unga o’qishga kirdim. Sizlarga ham shuni tavsiya qilaman. O’qishga kira olmadim" +
-            " deb tushkunlikka tushib" +
-            " o’tirmasdan xorijdagi nufuzli universitetlardan biriga “Akbar Consulting “ orqali" +
-            " o’qishga kiring.",
         img: "m3",
+        name: "Abdullayev Toshqinboy",
+        text: "Men O’sh Davlat Texnologiya unversiteti “Komputer texnologiyalari”" +
+            " fakultetiga topshirdim. “Akbar Consulting “ firmasi yordami bilan talabalikka erishdim" +
+            " va menga 1 yillik sug’urta polisini sovg’a sifatida taqdim qildi. " +
+            "Men o’qish istagida bo’lgan yoshlarni “Akbar consulting “ firmasiga murojaat qilishlarini" +
+            " tavsiya qilaman.",
         url: "https://www.youtube.com/watch?v=Wvj2mKqaPhQ"
     },
 ]
